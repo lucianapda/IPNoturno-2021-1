0a6ea6c4de49a680b56f9fecdb9d4ade1cdf3e37
lucianapda <lpa@furb.br>
2021-02-25 22:41:27 -0300

    Add files via upload

---
+++ unidade_1/resolucoes/EnunciadoB.java
@@ -0,0 +1,25 @@
+public class EnunciadoB {
+    public static void main(String[] args) {
+        //Modele a solução de um problema para calcular a área de uma sala
+        //retangular. Considere que as medidas estão expressas em metros.
+
+        //Enrolação: Modele a solução de um problema
+
+        //Entrada: lado1 e lado2
+        int lado1;
+        int lado2;
+
+        lado1 = 3;
+        lado2 = 90;
+
+        //Processo: lado1 * lado2
+        int area = lado1 * lado2;
+
+        //Saída: área
+        System.out.println("Área=" + area + "m²");
+        System.out.println("Área = " + area + " m²");
+        
+        System.out.println("Área = " + area);
+        System.out.println("Área=" + area);
+    }
+}
