62ee01e65487035a34a4564c984ded11b787d668
Dalton Solano dos Reis <dalton.reis@gmail.com>
2021-04-22 20:17:59 -0300

    Unidade 5.

---
+++ Unidade5/src/_Exemplo_1_FOR.java
@@ -0,0 +1,11 @@
+// contador++
+// contador+=1
+// contador-=1
+public class _Exemplo_1_FOR {
+	public static void main(String[] args) {
+		for (int contador = 1; contador <= 5  ; contador++  ) {
+			System.out.println(contador);
+		}
+		System.out.println("..FIM..");
+	}
+}
