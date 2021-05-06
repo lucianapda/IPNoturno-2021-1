59f6836125d957717788056fce5b79b22a0213f5
Dalton Solano dos Reis <dalton.reis@gmail.com>
2021-05-06 17:23:30 -0300

    Renomei a pasta.

--- Exemplos/src/Exemplo_String.java
+++ Exemplos/src/Exemplo_String.java
@@ -1,24 +1,22 @@
 import java.util.Scanner;
+
 public class Exemplo_String {
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
 
 		System.out.println("Informe 1a String: ");
 		String valorStr1 = teclado.next();
-
 		System.out.println("Informe 2a String: ");
 		String valorStr2 = teclado.next();
 
-		if (valorStr1.contentEquals(valorStr2)) {
-			System.out.println("Duas strings tem o mesmo valor: " + valorStr1);
+		if (valorStr1.equals(valorStr2)) {
+			System.out.println("Duas strings são iguais: " + valorStr1);
 		} else {
-			System.out.println("Duas strings tem valor diferentes: " + valorStr1 + " - " + valorStr2);
-
+			System.out.println("Duas strings são diferentes: " + valorStr1 + " - " + valorStr2);
 		}
 
 		System.out.println("Informe 1a número inteiro: ");
 		int valorInt1 = teclado.nextInt();
-
 		System.out.println("Informe 2a número inteiro: ");
 		int valorInt2 = teclado.nextInt();
 
@@ -28,8 +26,6 @@ public class Exemplo_String {
 		System.out.println(" Número com número numa string: " + strNumeros);
 
 		teclado.close();
-		System.out.println("..FIM..");
-
 	}
 
 }
